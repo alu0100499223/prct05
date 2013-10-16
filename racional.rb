d38583415bfd7d795031f6357e3f206c26ed4967
FERNANDEZ LUIS <alu3928@cc158.(none)>
2013-10-16 13:49:11 +0100

    gcb.rb corregido

---
+++ racional.rb
@@ -0,0 +1,26 @@
+# Implementar en este fichero la clase para crear objetos racionales
+
+require "./gcd.rb"
+require 'rational'
+
+class Fraccion
+    def to_s
+     
+    end
+    
+    def suma x, y
+      puts x + y
+    end
+ 
+    def resta x, y
+      puts x - y
+    end
+    
+    def producto x, y
+      puts x * y
+    end
+    
+    def división x, y
+      puts x / y
+    end
+end
